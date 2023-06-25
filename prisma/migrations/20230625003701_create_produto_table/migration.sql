4137120a8d50fea3b8c55f621b2af9b7eadac34a
Rafael Lawisch <lawisch.nh@gmail.com>
2023-06-24 21:37:37 -0300

    feat: reset data model to fix null issues

---
+++ prisma/migrations/20230625003701_create_produto_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Produto" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "cor" TEXT NOT NULL,
+    "marca" TEXT NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);

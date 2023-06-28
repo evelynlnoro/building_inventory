4a1627ecd61ca09e0c7a64aa6721218b5e44e083
I537293 <evelyn.noro@sap.com>
2023-06-28 10:52:43 -0300

    Resolve conflicts

--- app/materials/page.tsx
+++ app/materials/page.tsx
@@ -1,8 +1,14 @@
 import InventoryList from "@/app/materials/components/InventoryList";
+import Link from "next/link";
 
 export default async function MaterialsPage() {
   return (
     <>
+      <Link
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        href="/materials/new">
+        Novo material
+      </Link>
       <h1>Materiais</h1>
       <InventoryList />
     </>

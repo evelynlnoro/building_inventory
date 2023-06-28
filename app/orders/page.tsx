3e15def03b82edbb3beb5b8ebdef056116b3ee45
Rafael Lawisch <lawisch.nh@gmail.com>
2023-06-28 16:26:58 -0300

    chore: fix typo

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -3,7 +3,7 @@ import OrderList from "@/app/orders/components/OrderList";
 export default function OrdersPage() {
   return (
     <>
-      <h1>Pedidos</h1>;
+      <h1>Pedidos</h1>
       <OrderList />
     </>
   );
